4c2375a1393283710eea953b1f036b105ee59276
BrendanCampbell1 <144689653+BrendanCampbell1@users.noreply.github.com>
2023-09-11 10:37:43 -0400

    Update lab01.py

--- lab01.py
+++ lab01.py
@@ -2,11 +2,23 @@ def main():
     cost_per_item = 19.99
     quantity = 5 
 
-    # YOUR CODE TO CALCULATE THE TOTALS GOES HERE    
+    # YOUR CODE TO CALCULATE THE TOTALS GOES HERE 
+    #calculating subtotal
+    subtotal_cost = cost_per_item*quantity   
+
+    #calculating tax
+    tax = subtotal_cost*.13
+
+    #calculating total cost
+    total_cost = subtotal_cost+tax
 
     print(f'cost_per_item = ${cost_per_item:0.2f}') # a sample for you to use for the other prices
 
     # YOUR CODE TO PRINT THE TOTALS GOES HERE
+    print(f'quantity = {quantity}')
+    print(f'subtotal_cost = ${subtotal_cost:0.2f}')
+    print(f'tax = ${tax:0.2f}')
+    print(f'total_cost = ${total_cost:0.2f}')
 
 if __name__ == "__main__":
     main()
